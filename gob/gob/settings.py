b26628f3a8a67f5274ef845a817d663d40f6a468
Каманин Юрий <kamanchi22@mail.ru>
2023-04-28 18:56:02 +0300

    infra added

--- gob/gob/settings.py
+++ gob/gob/settings.py
@@ -113,6 +113,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
