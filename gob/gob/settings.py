bd0af5f8aab36d0c0abb1f3c1a1e6092bc76f762
Каманин Юрий <kamanchi22@mail.ru>
2023-05-27 12:25:32 +0300

    aruba first

--- gob/gob/settings.py
+++ gob/gob/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'data')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
