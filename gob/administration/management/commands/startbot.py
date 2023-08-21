5a95d4e044bff2add63c1287df62b35d5a25e439
Каманин Юрий <kamanchi22@mail.ru>
2023-08-21 15:48:04 +0300

    update modified to use list of keys

--- gob/administration/management/commands/startbot.py
+++ gob/administration/management/commands/startbot.py
@@ -21,6 +21,7 @@ def starts_bot() -> None:
     FSM_nearest_place.register_handlers_nearest_place(dp)
     admin.register_handlers_admin(dp)
     other.register_handlers_other(dp)
+
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 
 
