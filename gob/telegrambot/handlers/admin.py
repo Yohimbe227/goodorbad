1e060b70552cc51c5cdeca78f2786fd9b911fc14
Каманин Юрий <kamanchi22@mail.ru>
2023-08-30 15:37:01 +0300

    router to func register

--- gob/telegrambot/handlers/admin.py
+++ gob/telegrambot/handlers/admin.py
@@ -19,16 +19,16 @@ from telegrambot.moderator import IsCurseMessage
 from telegrambot.utils import send_message
 
 
-class FSMAdmin(StatesGroup):
+class Admin(StatesGroup):
     """Class of states."""
 
     city = State()
 
 
-router = Router()
+# router = Router()
 
 
-@router.message(F.from_user.id == ID, Command(commands=['загрузить', ]), )
+# @router.message(F.from_user.id == ID, Command(commands=['загрузить', ]), )
 async def city_add(message: types.Message, state: FSMContext) -> None:
     """
     Starting a dialog to load a new city.
@@ -43,13 +43,29 @@ async def city_add(message: types.Message, state: FSMContext) -> None:
         message,
         'Приветствую босс!!!',
     )
-    await state.set_state(FSMAdmin.city)
+    await state.set_state(Admin.city)
     await message.reply(
         'Введите город для добавления в базу',
     )
 
 
-@router.message(FSMAdmin.city, F.text, )
+# @router.message(F.text.lower().in_({'отмена', 'вернуться'}))
+async def cancel_handler(message: types.Message, state: FSMContext) -> None:
+    """Exit from the state.
+
+    Args:
+        message: Message being sent.
+        state: Current state.
+
+    """
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.clear()
+    await message.reply('OK', reply_markup=kb_client)
+
+
+# @router.message(FSMAdmin.city, F.text, )
 async def save_city_to_base(message: types.Message, state: FSMContext) -> None:
     """Process the first answer and write it in the dictionary.
 
@@ -63,31 +79,29 @@ async def save_city_to_base(message: types.Message, state: FSMContext) -> None:
     await state.clear()
 
 
-@router.message(F.text.lower().in_({'отмена', 'вернуться'}))
-async def cancel_handler(message: types.Message, state: FSMContext) -> None:
-    """Exit from the state.
+async def register_handlers_admin(disp: Dispatcher) -> None:
+    """
+    Handler registration.
 
     Args:
-        message: Message being sent.
-        state: Current state.
+        disp: Dispatcher object
 
     """
-    print('cancel handelr activated')
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    await state.clear()
-    await message.reply('OK', reply_markup=kb_client)
-
-
-# def register_handlers_admin(disp: Dispatcher) -> None:
-#     """
-#     Handler registration.
-#
-#     Args:
-#         disp: Dispatcher object
-
-    # """
+    disp.message.register(
+        city_add,
+        IsCurseMessage(),
+        F.text.in_({'загрузить'}),
+    )
+    disp.message.register(
+        cancel_handler,
+        IsCurseMessage(),
+        F.text.in_({'отмена', 'вернуться'}),
+    )
+    disp.message.register(
+        save_city_to_base,
+        Admin.city,
+        IsCurseMessage(),
+    )
     # disp.register_message_handler(
     #     city_add,
     #     IDFilter(user_id=ID),
