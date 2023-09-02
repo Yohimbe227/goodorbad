fb506d8be9441ddc2249b52d88298271fee29602
Каманин Юрий <kamanchi22@mail.ru>
2023-09-02 14:45:25 +0300

    подготовка тестов

---
+++ gob/administration/management/commands/simple.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Prints a greeting message'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--name', type=str, help='The name to greet')
+
+    def handle(self, *args, **options):
+        name = options['name']
+        if name:
+            self.stdout.write(f'Hello, {name}!')
+        else:
+            self.stdout.write('Hello, world!')
